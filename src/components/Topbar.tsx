290d2cef3a51e6703d17053935e946b6e9d29830
Mir-Labib-Hossain <mirlabibhossain99@gmail.com>
2024-03-13 19:03:09 +0600

    Project init

---
+++ src/components/Topbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const Topbar = (props: Props) => {
+  return (
+    <div className='max-w-[1320px] border' >Topbar</div>
+  )
+}
+
+export default Topbar
