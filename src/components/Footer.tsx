6696818f8c7b682fe13c4a5d54f7d97052a8ca02
Mir-Labib-Hossain <mirlabibhossain99@gmail.com>
2024-03-14 19:53:22 +0600

    Footer: small-display done

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 const Footer = () => {
   return (
-    <div className="h-[336px] bg-secondary flex flex-col gap-[78px] items-center justify-center">
-      <Image src="/images/footer-logo.svg" alt="footer" width={117} height={32} />
+    <div className="py-[69px] px-10 max-lg:py-10 bg-secondary flex flex-col gap-[78px] max-lg:gap-12 items-center justify-center">
+      <Image src="/images/footer-logo.svg" alt="footer" className="max-lg:w-[116px] max-lg:h-8" width={175} height={48} />
       <div className="flex flex-col items-center gap-6">
         <div className="flex gap-8">
           <Image src="/images/social-media/x-icon.svg" alt="x" width={24} height={24} />
@@ -11,7 +11,7 @@ const Footer = () => {
           <Image src="/images/social-media/facebook-icon.svg" alt="x" width={24} height={24} />
           <Image src="/images/social-media/dribble-icon.svg" alt="x" width={24} height={24} />
         </div>
-        <p className="text-base font-normal text-white">&#169; {new Date().getFullYear()} Lemonhive. All rights reserved.</p>
+        <p className="text-center text-base font-normal text-white">&#169; {new Date().getFullYear()} Lemonhive. All rights reserved.</p>
       </div>
     </div>
   );
