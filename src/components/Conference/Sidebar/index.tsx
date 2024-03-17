231de0feb827f34877d81b82760f2bd10ab3b199
Mir-Labib-Hossain <mirlabibhossain99@gmail.com>
2024-03-17 16:24:56 +0600

    Working on - DnD

--- src/components/Conference/Sidebar/index.tsx
+++ src/components/Conference/Sidebar/index.tsx
@@ -1,19 +1,53 @@
+import { useState } from "react";
 import ConferenceLink from "./ConferenceLink";
-
 type Props = {
   index: string;
 };
 
 const Sidebar = ({ index }: Props) => {
+  const [list, setList] = useState([
+    { id: 0, url: `/conference/organizer?index=${index}`, title: "Organizer" },
+    { id: 1, url: `/conference/schedule?index=${index}`, title: "Schedule" },
+    { id: 2, url: `/conference/speakers?index=${index}`, title: "Speakers" },
+    { id: 3, url: `/conference/sponsors?index=${index}`, title: "Sponsors" },
+  ]);
+
+  const [draggedItem, setDraggedItem] = useState(null);
+
+  const handleDragStart = (e, id) => {
+    e.dataTransfer.setData("id", id);
+    setDraggedItem(id);
+  };
+
+  const handleDragOver = (e, id) => {
+    e.preventDefault();
+    const draggedOverItem = list.find((task) => task.id === id);
+    if (draggedItem === draggedOverItem) {
+      return;
+    }
+    let items = list.filter((task) => task.id !== draggedItem);
+    let index = list.indexOf(draggedOverItem);
+    items.splice(
+      index,
+      0,
+      list.find((task) => task.id === draggedItem)
+    );
+    setList(items);
+  };
+
+  const handleDragEnd = () => {
+    setDraggedItem(null);
+  };
+
   return (
     <div className="flex flex-col gap-8">
-      <ConferenceLink href={`/conference/organizer?index=${index}`}>Organizer</ConferenceLink>
-
-      <ConferenceLink href={`/conference/schedule?index=${index}`}>Schedule</ConferenceLink>
-      <ConferenceLink href={`/conference/speakers?index=${index}`}>Speakers</ConferenceLink>
-      <ConferenceLink href={`/conference/sponsors?index=${index}`}>Sponsors</ConferenceLink>
+      {list.map(({ id, url, title }) => (
+        <div key={id} className="task transition-transform duration-500 ease-in-out" draggable onDragStart={(e) => handleDragStart(e, id)} onDragOver={(e) => handleDragOver(e, id)} onDragEnd={handleDragEnd}>
+          <ConferenceLink key={url} href={url}>
+            {title}
+          </ConferenceLink>
+        </div>
+      ))}
     </div>
   );
-};
-
-export default Sidebar;
+};export default Sidebar;
