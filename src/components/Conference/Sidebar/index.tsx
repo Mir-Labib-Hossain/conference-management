2c2ddde60ac40f2a8b93d43fbdf0c8365b926560
Mir-Labib-Hossain <mirlabibhossain99@gmail.com>
2024-03-17 15:41:57 +0600

    Working on - DnD

--- src/components/Conference/Sidebar/index.tsx
+++ src/components/Conference/Sidebar/index.tsx
@@ -4,11 +4,11 @@ type Props = {
   index: string;
 };
 
-const Sidebar = ({index}: Props) => {
+const Sidebar = ({ index }: Props) => {
   return (
     <div className="flex flex-col gap-8">
-        
       <ConferenceLink href={`/conference/organizer?index=${index}`}>Organizer</ConferenceLink>
+
       <ConferenceLink href={`/conference/schedule?index=${index}`}>Schedule</ConferenceLink>
       <ConferenceLink href={`/conference/speakers?index=${index}`}>Speakers</ConferenceLink>
       <ConferenceLink href={`/conference/sponsors?index=${index}`}>Sponsors</ConferenceLink>
